856e4edf4c62fc9d5f4c9579016b3f0cbcd647c8
SOUSIC <517048082@qq.com>
2018-11-16 20:37:30 +0800

    Update dragonball.py

--- dragonball.py
+++ dragonball.py
@@ -24,31 +24,35 @@ user_agent = [
 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
 	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
 	]
+
+#代理
 http_ip = [
-	'',#不用代理
-	'112.95.20.27:8888',
-	'219.159.38.198:56210',
-	]
+		'',#不用代理
+		#'112.95.20.27:8888',
+		#'219.159.38.198:56210',
+		]
 https_ip = [
-	'',
-	]
+		'',
+		]
 '''
 1、当你的Server内存充足时，KeepAlive =On还是Off对系统性能影响不大。
 2、当你的Server上静态网页(Html、图片、Css、Js)居多时，建议打开KeepAlive 。
 3、当你的Server多为动态请求(因为连接数据库，对文件系统访问较多)，KeepAlive 关掉，会节省一定的内存，节省的内存正好可以作为文件系统的Cache(vmstat命令中cache一列)，降低I/O压力。
 PS：当KeepAlive =On时，KeepAliveTimeOut的设置其实也是一个问题，设置的过短，会导致Apache 频繁建立连接，给Cpu造成压力，设置的过长，系统中就会堆积无用的Http连接，消耗掉大量内存，具体设置多少，可以进行不断的调节，因你的网站浏览和服务器配置 而异。
 '''
+#请求头
 headers = {
-	#'Accept': 'text/html, application/xhtml+xml, image/jxr, */*',
-	#'Accept - Encoding':'gzip, deflate',
-	#'Accept-Language':'zh-Hans-CN, zh-Hans; q=0.5',
-	'Connection':'Keep-Alive',#'close'
-	#'Host':'',
-	'User-Agent':random.choice(user_agent)
+	'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
+	'Accept - Encoding':'gzip, deflate',
+	'Accept-Language':'zh-CN,zh;q=0.9,zh-TW;q=0.8,ja;q=0.7',
+	'Connection':'close',#'Keep-Alive'容易被ban IP
+	'Host':'comic.dragonballcn.com',
+	'Referer':'',
+	'User-Agent':random.choice(user_agent),
 	}
 proxies= {#该网站是http协议
 	"http":random.choice(http_ip),
-	#"https":random.choice(https_ip)
+	"https":random.choice(https_ip)
 		}
 
 #从html中提取小图url
@@ -62,29 +66,29 @@ def get_thumb_url(origi_url):
 		urls.append(img_url)
 	return urls
 
-
 # 处理url，得到大图url
 def get_big_url(thumb_urls):
-	urls = []
-	for thumb_url in thumb_urls:
-		split_url = thumb_url.split('/')
-		# 去掉列表中的空元素
-		while '' in split_url:
-		    split_url.remove('')
-		# 观察大图的url和小图的url的区别，把列表最后一个元素改一下
-		# 以'.'分离之后把第一个.thumb去掉再用'.'连接起来
-		big_url = '.'.join(split_url[-1].split('.')[1:])
-		split_url = split_url[1:-1]
-		split_url.append(big_url)
-		url = '/'.join(split_url)
-		big_url = 'http://' + url
-		urls.append(big_url)
-	return urls
+    urls = []
+    for thumb_url in thumb_urls:
+        split_url = thumb_url.split('/')
+        # 去掉列表中的空元素
+        while '' in split_url:
+            split_url.remove('')
+        # 观察大图的url和小图的url的区别，把列表最后一个元素改一下
+        # 以'.'分离之后把第一个.thumb去掉再用'.'连接起来
+        big_url = '.'.join(split_url[-1].split('.')[1:])
+        split_url = split_url[1:-1]
+        split_url.append(big_url)
+        url = '/'.join(split_url)
+        big_url = 'http://' + url
+        urls.append(big_url)
+    return urls
 
 #用大图url下载到对应的文件夹中
-def img_downloader(urls):
+def img_downloader(urls,headers_page,vol):
 	totalurl = len(urls)
-	i = 0
+	i = 0	#用来算完成度
+	j = 0	#用来掌控请求速度
 	for url in urls:
 		i += 1
 		pencent = i/totalurl
@@ -100,21 +104,23 @@ def img_downloader(urls):
 			os.mkdir(root)          #创建根目录
 		if not os.path.exists(document):
 			os.mkdir(document)      #创建卷的文件夹
-		if not os.path.exists(path):#判断文件是否存在		
+		if not os.path.exists(path):#判断文件是否存在
 			#######应该放在判断文件不存在后请求
-			req = requests.get(url,headers=headers,proxies=proxies,timeout=60)
-			#####################################
+			#每请求一次页面i+1
+			j += 1
+			req = requests.get(url,headers=headers_page,proxies=proxies,timeout=60)
+			############
 			with open(path,'wb')as f:
 				f.write(req.content)
 				f.close()
-				print("成功,已完成%.2f" % (pencent*100) + '%')
+				print("成功 " + " --第 %d 卷" % vol + "已完成 %.2f" % (pencent*100) + '%')
+			#请求一次页面暂停一会儿
+			time.sleep(random.random() * 10)
 		else:
-			print("已存在,已完成%.2f" % (pencent*100) + '%')
-		time.sleep(random.random() * 2)
-		#pause per 10 scrawl
-		if i//10 != 0:
-			time.sleep(5 + random.random() * 5)
-
+			print("已存在 " + " --第 %d 卷" % vol + "已完成 %.2f" % (pencent*100) + '%')
+		#每10此请求，多暂停一会儿
+		if j//10 != 0:
+			time.sleep(10 + random.random() * 50)
 
 def main():
 	urls = []
@@ -124,12 +130,15 @@ def main():
 		"http://comic.dragonballcn.com/list/gain_1.php?did=0-9-2",
 		]
 	for url in start_urls:
+		fragment = url.split('-')
+		page = int(fragment[-1]) + 1
 		thumb_urls = get_thumb_url(url)
 		big_urls = get_big_url(thumb_urls)
-		urls.extend(big_urls)
-	#print(urls)
-	img_downloader(urls)
-		
+		print('第 %d 卷链接获取完毕，开始下载图片：' %page)
+		headers_page = headers
+		headers_page['Referer'] = url
+		#print(headers_page)
+		img_downloader(big_urls,headers_page,page)
 
 if __name__ == '__main__':
 	main()	
